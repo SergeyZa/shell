d9c32ab28910e8d8081e160534f3ad9207a03d19
Sergey <sergey@orangesoftware.com>
2024-02-26 14:53:40 -0800

    added profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+Import-Module -Name Terminal-Icons
+
+oh-my-posh --init --shell pwsh --config C:\projects\shell\sergeyz.omp.json | Invoke-Expression
+
+function ??
+{
+    gh copilot suggest -t shell @args
+}
+
+function ps?
+{
+    gh copilot suggest -t shell 'use powershell to ' + @args
+}
+
+function git?
+{
+    gh copilot suggest -t git @args
+}
+
+function gh?
+{
+    gh copilot suggest -t gh @args
+}
